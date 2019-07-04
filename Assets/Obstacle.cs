614eacc49283cc611f54cf5d593c7ccbb81f123b
oboking <boking@student.chalmers.se>
2019-07-04 14:46:09 +0200

    Adds obstacle spawner
    penus

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public float speed;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+	    other.GetComponent<Player>().isAlive = false;
+	    Destroy(gameObject);
+	} else if (other.CompareTag("Cleanup")) {
+	    Destroy(gameObject);
+	}
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+	Debug.Log("onstart");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	
+    public GameObject[] obstaclePatterns;
+
+    private float timeBtwSpawn;
+
+    public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime = 0.65f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwSpawn <= 0) {
+	    int rand = Random.Range(0, obstaclePatterns.Length);
+	    Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
+	    timeBtwSpawn = startTimeBtwSpawn;
+	    if (startTimeBtwSpawn > minTime) {
+	        startTimeBtwSpawn -= decreaseTime;
+	    }
+	} else {
+	    timeBtwSpawn -= Time.deltaTime;
+	}
+    }
+}
